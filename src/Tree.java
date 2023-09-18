b2e3dc39f4b6c4ca7938d2173b0454f498e9a58f
Junhee <junheelee.lee@mail.utoronto.ca>
2023-09-18 12:59:12 -0400

    2 methods in Tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,18 @@
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public class Tree {
+    private Optional<Object>[] _root;
+    private Tree[] _subtrees;
+
+    public Tree(Optional<Object>[] root, Optional<Tree[]> subtrees) {
+        _root = root;
+        if (subtrees.isPresent()) {
+            _subtrees = subtrees.get();
+        }
+    }
+
+    public boolean is_empty() {
+        return _root.length == 0;
+    }
 }

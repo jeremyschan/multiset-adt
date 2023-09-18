80a03284a63ccb767acf778515bd0083582161e4
jeremyschan <jeremych.chan@mail.utoronto.ca>
2023-09-18 12:48:51 -0400

    multiset class created

---
+++ src/Multiset.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Multiset {
+    public static void add(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void remove(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void contains(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void is_empty(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void count(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void size(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+}
